40c44d5281f006bd094c312d151438318e764d94
Jason Hejna <jason.hejna@gmail.com>
2016-11-07 19:44:52 -0500

    +1logging

--- was_flicked.py
+++ was_flicked.py
@@ -35,14 +35,14 @@ class DetermineSwitch:
             self.averages.append(self.average_light_level)
             self.readings = []
             self.i = 0
-            if len(self.averages) > 600:
+            if len(self.averages) > 200:
                 self.averages.pop(0)
                 self.check_for_flick()
 
     def check_for_flick(self):
         #print 'self.averages', self.averages
         detected_flicks = 1
-        for i in range(0, 599):
+        for i in range(0, 199):
             #print self.averages[i], ' - ', self.averages[i + 1], ' = ', self.averages[i] - self.averages[i + 1]
             diff1 = abs(self.averages[i] - self.averages[i + 1])
             if diff1 > 1700:
@@ -56,8 +56,8 @@ class DetermineSwitch:
         while True:
             #time1 = time.time()
             self.store_reads(read.get_light_level())
-            #print 'T:', time.time() - time1
             time.sleep(0.01)
+            #print 'T:', time.time() - time1
 
     def unit_test(self):
         test_list = open('./unit_tests/unit_test_1').read().splitlines()
