1daeb2f2f98959d5da973a78a0de6337db3452dc
Jason Hejna <jason.hejna@gmail.com>
2016-11-12 12:30:35 -0500

    added clean exit

--- example_functions/relay.py
+++ example_functions/relay.py
@@ -2,7 +2,7 @@
 
 # Turn on a relay for N seconds
 
-import RPi.GPIO as GPIO, time
+import RPi.GPIO as GPIO, time, atexit
 
 
 def turn_on_for(set_time):
@@ -13,4 +13,10 @@ def turn_on_for(set_time):
     GPIO.output(23, GPIO.LOW)
     GPIO.cleanup()
 
+def exit_handler():
+    GPIO.output(23, GPIO.LOW)
+    GPIO.cleanup()
+    print 'exiting'
+
+atexit.register(exit_handler)
 turn_on_for(30)
