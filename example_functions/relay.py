6628d15ea78a23f1ca3d9e33514401a3ade8cdfe
Jason Hejna <jason.hejna@gmail.com>
2016-11-12 12:54:55 -0500

    added threading

--- example_functions/relay.py
+++ example_functions/relay.py
@@ -2,7 +2,9 @@
 
 # Turn on a relay for N seconds
 
-import RPi.GPIO as GPIO, time, atexit
+# TODO: The Thread need to shut itself down gracefully
+
+import RPi.GPIO as GPIO, threading, time, atexit
 
 
 def turn_on_for(set_time):
@@ -13,10 +15,15 @@ def turn_on_for(set_time):
     GPIO.output(16, GPIO.LOW)
     GPIO.cleanup()
 
+
 def exit_handler():
     GPIO.output(16, GPIO.LOW)
     GPIO.cleanup()
     print 'exiting'
 
 atexit.register(exit_handler)
-turn_on_for(30)
+
+thread = threading.Thread(target=turn_on_for, args=(20))
+thread.start()
+thread.join()
+print "thread finished...exiting"
