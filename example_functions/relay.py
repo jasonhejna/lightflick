2a04cba14dd5daaa52867d94e0e4ae946531ec4f
Jason Hejna <jason.hejna@gmail.com>
2016-11-12 12:33:53 -0500

    changed pin

--- example_functions/relay.py
+++ example_functions/relay.py
@@ -7,14 +7,14 @@ import RPi.GPIO as GPIO, time, atexit
 
 def turn_on_for(set_time):
     GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(23, GPIO.OUT)
-    GPIO.output(23, GPIO.HIGH)
+    GPIO.setup(16, GPIO.OUT)
+    GPIO.output(16, GPIO.HIGH)
     time.sleep(set_time)
-    GPIO.output(23, GPIO.LOW)
+    GPIO.output(16, GPIO.LOW)
     GPIO.cleanup()
 
 def exit_handler():
-    GPIO.output(23, GPIO.LOW)
+    GPIO.output(16, GPIO.LOW)
     GPIO.cleanup()
     print 'exiting'
 
