15ac25c7dfd8b8d1b09678f88d401a435f317fb4
Jason Hejna <jason.hejna@gmail.com>
2016-11-12 13:21:37 -0500

    thread testing

--- example_functions/relay.py
+++ example_functions/relay.py
@@ -18,7 +18,7 @@ def turn_on_for(on_for_time):
 
 
 def start(input_time):
-    thread = threading.Thread(target=turn_on_for, args=[input_time]).start()
-    return thread.isAlive()
+    thread = threading.Thread(target=turn_on_for, args=[input_time])
+    thread.start()
 
 start(20)
