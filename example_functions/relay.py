f46f44f7711dc669f39dcb9ed7eef110ae1a448a
Jason Hejna <jason.hejna@gmail.com>
2016-11-12 12:59:17 -0500

    gpio bug

--- example_functions/relay.py
+++ example_functions/relay.py
@@ -7,16 +7,16 @@
 import RPi.GPIO as GPIO, threading, time
 
 
-def turn_on_for(set_time):
+def turn_on_for():
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(16, GPIO.OUT)
     GPIO.output(16, GPIO.HIGH)
-    time.sleep(set_time)
+    time.sleep(20)
     GPIO.output(16, GPIO.LOW)
     GPIO.cleanup()
 
 
-thread = threading.Thread(target=turn_on_for, args=(20))
+thread = threading.Thread(target=turn_on_for)
 thread.start()
 thread.join()
 print "thread finished...exiting"
